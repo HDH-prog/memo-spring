e2596a2558b7b70d3251bb6cc653a4df63039652
green <eastking34@gmail.com>
2026-07-09 17:14:33 +0900

    메모 저장 구현

--- src/main/java/com/service/MemoService.java
+++ src/main/java/com/service/MemoService.java
@@ -1,12 +1,29 @@
 package com.service;
 
+import com.dto.CreateMemoRequest;
+import com.dto.CreateMemoResponse;
+import com.entity.Memo;
 import com.repository.MemoRepository;
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemoService {
+
     private final MemoRepository memoRepository;
+
+    @Transactional
+    public CreateMemoResponse save(CreateMemoRequest request) {
+        Memo memo = new Memo(
+                request.getTitle(),
+                request.getContent()
+        );
+        Memo savedMemo = memoRepository.save(memo);
+        return new CreateMemoResponse(
+                savedMemo.getId(),
+                savedMemo.getTitle(),
+                savedMemo.getContent());
+    }
 }
